b7eca3242e5bc508c5fed09d9691932962aa5419
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 16:16:23 +0600

    racap complete

--- recap.js
+++ recap.js
@@ -1,3 +1,9 @@
 function doMath(num1, num2) {
-
+    const sum = num1 + num2;
+    const diff = num1 - num2;
+    const multiply = sum * diff;
+    const result = multiply / 2;
+    return result
 }
+const finalResult = doMath(8, 5)
+console.log(finalResult);
