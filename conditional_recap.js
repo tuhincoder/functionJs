7f8951f03b386732f6bec29940f5c305336d808e
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 18:39:03 +0600

    conditionnal_recap

---
+++ conditional_recap.js
@@ -0,0 +1,75 @@
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(isEven(6));
+
+// function isEven(number) {
+//     if (number % 2 === 0) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+
+// console.log(isEven(10));
+
+// function isOdd(number) {
+//     if (number % 2 === 1) {
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+// }
+// console.log(isOdd(10));
+
+// function isCondition(number) {
+//     if (number % 2 === 1) {
+//         return true
+//     }
+//     return false
+// }
+
+// console.log(isCondition(10));
+// console.log(isCondition(9));
+
+
+
+// function evenStr(str) {
+//     const size = str.length;
+//     console.log(size);
+//     if (size % 2 === 0) {
+//         console.log('even size');
+//         return true
+//     }
+//     else {
+//         console.log('odd size');
+//         return false
+//     }
+// }
+
+// console.log(evenStr('dhaka'));
+// console.log(evenStr('khali'));
+
+function evenStr(str) {
+    const size = str.length;
+    console.log(size);
+    if (size % 2 === 0) {
+        console.log('even str');
+        return true
+    }
+    else {
+        console.log('odd str');
+        return false
+    }
+}
+
+console.log(evenStr('dhakadfadsf'));
+console.log(evenStr('uttaraaa'));
