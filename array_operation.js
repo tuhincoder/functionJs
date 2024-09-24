76529e170a0aafcc5d8b43dd450552f97c46881d
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 18:57:02 +0600

    array operation

---
+++ array_operation.js
@@ -0,0 +1,30 @@
+// function someOfNumbers(number) {
+//     let sum = 0;
+//     for (const num of number) {
+//         sum = sum + num;
+//     }
+//     // console.log(sum);
+//     return sum
+// }
+// const numArray = [1, 2, 3, 4, 5]
+// const numberOf = someOfNumbers(numArray)
+// console.log(numberOf);
+
+// function someEven(number) {
+//     let sum = 0;
+//     for (const nub of number) {
+//         sum = sum + nub
+//     }
+//     // console.log(sum);
+//     return sum
+// }
+
+// const evenArray = [1, 2, 3, 4, 5, 6, 7]
+// const numberOf = someEven(evenArray)
+// console.log(numberOf);
+
+// function numberOfEven(number){
+
+// }
+
+// const 
