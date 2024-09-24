44951d3bb4c0ecefefe7c815200307655f202fd5
tuhincoder <mdtuhinmahmudul@gmail.com>
2024-09-24 11:53:45 +0600

    function complete

---
+++ function.js
@@ -0,0 +1,60 @@
+// function rice() {
+//     console.log('i will eat rice');
+//     console.log('i will not eat rice');
+// }
+// rice()
+
+// -----------------
+// function square(numb) {
+//     const borgo = numb * numb;
+//     console.log(borgo);
+// }
+// square(4)
+
+// function add(num1, num2) {
+//     const add = num1 + num2;
+//     console.log(add);
+// }
+// add(3, 5)
+// add(10, 5)
+
+
+// function addAll(a, b, c, d, e) {
+//     const allAdded = a + b + c + d + e;
+//     console.log(allAdded);
+// }
+// addAll(2, 4, 5, 3, 6)
+
+
+// function double(number) {
+//     const dob = number * 2;
+//     console.log(dob);
+// }
+// console.log('i will call the function');
+// double(5)
+// double(17)
+// double(40)
+
+// function difference(num1, numb2) {
+//     const diff = num1 - numb2;
+//     console.log(diff);
+// }
+// const fatherAge = 40;
+// const myAge = 20;
+// difference(fatherAge, myAge)
+
+function teTimes(number) {
+    const result = number * 10;
+    return result;
+}
+
+const mulTimes = teTimes(5)
+console.log('multiple kore paichi', mulTimes);
+
+function cutHalf(number) {
+    const half = number / 2;
+    return half
+}
+
+const division = cutHalf(10)
+console.log('division kore paichi', division);
